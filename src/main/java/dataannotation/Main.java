e32d1cf5e3d007dba8c903d4a212fb8a12119e80
Vincent Ngan <tngan@my.centennialcollege.ca>
2024-04-26 09:10:07 -0400

    Initial commit

---
+++ src/main/java/dataannotation/Main.java
@@ -0,0 +1,13 @@
+package dataannotation;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        decode("coding_qual_input.txt");
+    }
+
+    public static void decode(String fileName) throws Exception {
+
+    }
+
+}
